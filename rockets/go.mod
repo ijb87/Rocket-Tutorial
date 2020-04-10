b2b267205fe3f7a6a0fe58db31eb5351b01a1a2e
iaihabjb <ihab@ihab-baghdadi.com>
2020-04-10 14:32:27 -0400

    update

---
+++ rockets/go.mod
@@ -0,0 +1,8 @@
+module github.com/GetStream/rockets-go-tutorial
+
+go 1.14
+
+require (
+	github.com/esimov/caire v1.2.5
+	github.com/pkg/errors v0.9.1
+)
---
+++ rockets/main.go
@@ -0,0 +1,30 @@
+package main
+import (
+    "bytes"
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+    "github.com/GetStream/rockets-go-tutorial/seam"
+)
+const (
+    IMAGE_URL string = "https://bit.ly/2QGPDkr"
+)
+
+func main() {
+    fmt.Println("Ready for liftoff! Checkout http://localhost:3000/occupymars")
+    http.HandleFunc("/occupymars", func(w http.ResponseWriter, r *http.Request) {
+        if r.URL.Query().Get("resize") > "" {
+            resized, err := seam.ContentAwareResize(IMAGE_URL)
+            if err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+            }
+            w.Header().Set("Content-Type", "image/jpeg")
+            io.Copy(w, bytes.NewReader(resized))
+        } else {
+            fmt.Fprintf(w, "<html><div>Original image:</div> <img src=\"%s\" /><br/><a href=\"?resize=1\">Resize using Seam Carving</a></html>", IMAGE_URL)
+        }
+    })
+    log.Fatal(http.ListenAndServe(":3000", nil))
+}
